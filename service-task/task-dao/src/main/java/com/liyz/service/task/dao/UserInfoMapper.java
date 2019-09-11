5250ab453b69293eb68a62e406b0eab1c031398c
liyangzhen@ichainwin.com <Aa123456>
2019-09-11 21:00:01 +0800

    add

---
+++ service-task/task-dao/src/main/java/com/liyz/service/task/dao/UserInfoMapper.java
@@ -0,0 +1,14 @@
+package com.liyz.service.task.dao;
+
+import com.liyz.common.dao.mapper.Mapper;
+import com.liyz.service.task.model.UserInfoDO;
+
+/**
+ * 注释:
+ *
+ * @author liyangzhen
+ * @version 1.0.0
+ * @date 2019/8/28 17:50
+ */
+public interface UserInfoMapper extends Mapper<UserInfoDO> {
+}
